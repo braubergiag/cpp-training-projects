a1cf7359ff3bb6128927638adec49b00fa583d35
Igor Glushkov <braubergiag@gmail.com>
2022-12-11 17:34:49 +0300

    tropical init method module

---
+++ tropical/include/method.h
@@ -0,0 +1,8 @@
+//
+// Created by igor on 11.12.22.
+//
+
+#ifndef TROPICAL_METHOD_H
+#define TROPICAL_METHOD_H
+
+#endif //TROPICAL_METHOD_H
---
+++ tropical/src/method.cpp
@@ -0,0 +1,3 @@
+//
+// Created by igor on 11.12.22.
+//
